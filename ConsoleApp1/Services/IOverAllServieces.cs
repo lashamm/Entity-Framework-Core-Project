48a553e85f003cba0e116078cf28d0e6d6c22183
lasha <maisuradze.llasha@gmail.com>
2026-01-15 18:45:23 +0400

    made crud services

---
+++ ConsoleApp1/Services/IOverAllServieces.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Services
+{
+    internal interface IOverAllServieces<T> where T : class
+    {
+
+        Task<List<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        Task AddAsync(T Variable);
+        Task UpdateAsync(T Variable);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ ConsoleApp1/Services/OverAllServices.cs
@@ -0,0 +1,47 @@
+﻿using ConsoleApp1.Data;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Services
+{
+    internal class OverAllServices : IOverAllServieces
+    {
+        private readonly ItStepProjectContext context = new ItStepProjectContext();
+
+        public async Task AddAsync<T>(T Variable) where T : class
+        {
+            await context.AddAsync(Variable);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync<T>(int id) where T : class
+        {
+            await context.FindAsync<T>(id);
+
+            context.Remove(id);
+
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<List<T>> GetAllAsync<T>() where T : class
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public async Task<T?> GetByIdAsync<T>(int id) where T : class
+        {
+            return await context.FindAsync<T>(id);
+        }
+
+        public async Task UpdateAsync<T>(T Variable) where T : class
+        {
+            await context.FindAsync<T>(Variable);
+            await context.SaveChangesAsync();
+        }
+    }
+}
