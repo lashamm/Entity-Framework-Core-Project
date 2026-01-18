8be5d8a58e2065ecec48089500bbee0f1189bfea
lasha <maisuradze.llasha@gmail.com>
2026-01-18 14:15:35 +0400

    Will continue

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Welcome Press S to start");
+var key = Console.ReadKey();
+if (key.Key == ConsoleKey.S)
+{
+    Console.WriteLine("\nStarting the process...");
+    Console.Clear();
+    Console.WriteLine($"Press 1 to edit Branch" +
+        $"\tPress 2 to edit Brand" +
+        $"\tPress 3 to edit City" +
+        $"\tPress 4 to edit Model" +
+        $"\tPress 5 to edit Product" +
+        $"\tPress 6 to edit Product Category" +
+        $"\tPress 7 to edit Product Title" +
+        $"\tPress 8 to edit Street");
+}
+else
+{
+    Console.WriteLine("\nInvalid key pressed. Exiting...");
+}
